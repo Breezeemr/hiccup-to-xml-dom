91d70d7a123d0d6ff9f7a4fd859555a944e4cf0a
Tyler Tallman <ttallman@breezeehr.com>
2021-07-01 16:07:47 -0500

    enable schema validation

--- src/hiccup_to_xml_dom/core.clj
+++ src/hiccup_to_xml_dom/core.clj
@@ -1,13 +1,111 @@
 (ns hiccup-to-xml-dom.core
-  (:import [javax.xml.parsers DocumentBuilder DocumentBuilderFactory]))
+  (:import [javax.xml.parsers DocumentBuilder DocumentBuilderFactory]
+           (java.io StringWriter)
+           (javax.xml.transform.dom DOMSource)
+           (javax.xml.transform TransformerFactory)
+           (javax.xml.transform.stream StreamResult)))
+
+(defprotocol AsElements
+  (as-elements [expr doc doc-ns] "Return a seq of elements represented by an expression."))
 
 ;; (def doc-ns "http://www.w3.org/1999/XSL/Format")
+(defn element* [tag attrs content doc doc-ns]
+  (let [el (. doc (createElementNS doc-ns (name tag)))
+        as-el-wrapper #(as-elements % doc doc-ns)]
+    (doseq [[k v] attrs]
+      (.setAttributeNS el (namespace k) (name k) v))
+    (transduce
+      (map identity)
+      (fn
+        ([acc] acc)
+        ([acc item]
+         (cond
+           (string? item) (.appendChild
+                            el
+                            (.createTextNode doc item))
+           (not (nil? item)) (.appendChild el item))))
+      nil
+      (as-elements content doc doc-ns))
+    el))
+
+(defn sexp-element [tag attrs child doc doc-ns]
+  (cond
+    ;(= :-cdata tag) (cdata (first child))
+    ;(= :-comment tag) (xml-comment (first child))
+    :else (element* tag attrs child  doc doc-ns)))
+
+(extend-protocol AsElements
+  clojure.lang.IPersistentVector
+  (as-elements [v doc doc-ns]
+    (let [[tag & [attrs & after-attrs :as content]] v
+          [attrs content] (if (map? attrs)
+                            [(into {} (for [[k v] attrs]
+                                        [k (str v)]))
+                             after-attrs]
+                            [{} content])]
+      [(sexp-element tag attrs content  doc doc-ns)]))
+
+  clojure.lang.ISeq
+  (as-elements [s doc doc-ns]
+    (let [as-el-wrapper #(as-elements % doc doc-ns)]
+      (mapcat as-el-wrapper s)))
+
+  ;clojure.lang.Keyword
+  ;(as-elements [k doc doc-ns]
+  ;  [(element* k nil nil doc doc-ns)])
+
+  java.lang.String
+  (as-elements [s doc doc-ns]
+    [s])
+
+  nil
+  (as-elements [_ doc doc-ns] nil)
+
+  java.lang.Object
+  (as-elements [o doc doc-ns]
+    [(str o)]))
+
+(defn sexps-as-fragment
+  "Convert a compact prxml/hiccup-style data structure into the more formal
+   tag/attrs/content format. A seq of elements will be returned, which may
+   not be suitable for immediate use as there is no root element. See also
+   sexp-as-element.
+
+   The format is [:tag-name attr-map? content*]. Each vector opens a new tag;
+   seqs do not open new tags, and are just used for inserting groups of elements
+   into the parent tag. A bare keyword not in a vector creates an empty element.
+
+   To provide XML conversion for your own data types, extend the AsElements
+   protocol to them."
+  ([doc doc-ns] nil)
+  ([doc doc-ns sexp]
+   (as-elements sexp doc doc-ns)))
+
+(defn sexp-as-element
+  "Convert a single sexp into an Element"
+  [doc doc-ns sexp]
+  (let [[root & more] (sexps-as-fragment doc doc-ns sexp)]
+    (when more
+      (throw
+        (IllegalArgumentException.
+          "Cannot have multiple root elements; try creating a fragment instead")))
+    root))
+
 
 (defn serialize-dom [dom]
   (let [ls-impl (.. dom (getOwnerDocument) (getImplementation) (getFeature "LS" "3.0"))
         serializer (.createLSSerializer ls-impl)]
     (.. serializer (getDomConfig) (setParameter "xml-declaration" false))
     (.writeToString serializer dom)))
+
+(defn serialize-dom2 [dom]
+  (let [source (DOMSource. dom)
+        sw     (StringWriter.)
+        sr     (StreamResult. sw)
+        txformer (-> (TransformerFactory/newDefaultInstance)
+                 .newTransformer)]
+    (.transform txformer source sr)
+    (str sw)))
 
 (defn- create-elem-ns [doc doc-ns tag attrs content]
   (let [el (. doc (createElementNS doc-ns (name tag)))
@@ -63,3 +161,9 @@
   (let [db (make-document-builder)
         doc-root (.newDocument db)]
     (compile-element (into [doc-root] content) document-namespace)))
+
+(defn ->dom2 [content & {:keys [document-namespace] :or {document-namespace ""}}]
+  (let [db (make-document-builder)
+        doc-root (.newDocument db)]
+    (.appendChild doc-root (sexp-as-element doc-root document-namespace content))
+    doc-root))
